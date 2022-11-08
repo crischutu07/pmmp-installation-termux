359c0d27837cedef1dac0dba04cae3f85b7bb145
Nguyễn Hồng Sơn <81855366+crischutu07@users.noreply.github.com>
2022-11-08 09:50:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,30 @@
 #!/data/data/com.termux/files/usr/bin/bash
+PMMP_VER="4.10.11"
+PHP_VER="8.0.22"
+if ! command -v curl &> /dev/null; then
+  echo "[*] Command curl not found"
+  exit 1
+fi
+if ! command -v getconf &> /dev/null; then
+  echo "[*] Command getconf not found"
+  exit 1
+fi
+
+if [ `getconf LONG_BIT` == "32" ]; then
+	echo "[*] PocketMine-MP is no longer supported on 32-bit systems."
+	exit 1
+fi
+
+mkdir -p ./bin/php7/bin/
+echo "[*] Installing PHP 8.0.22 Binary"
+curl -s -o php https://github.com/DaisukeDaisuke/AndroidPHP/releases/download/{PHP_VER}/php
+mv php ./bin/php7/bin
+curl -s -o php.ini https://github.com/DaisukeDaisuke/AndroidPHP/releases/download/{PHP_VER}/php-pm4.ini
+mv php.ini ./bin/php7/bin/
+chmod +x ./bin/php7/bin/php
+echo -e "[*] Installing PocketMine-MP {PMMP_VER}"
+curl -s -o PocketMine-MP.phar https://github.com/pmmp/PocketMine-MP/releases/download/{PMMP_VER}/PocketMine-MP.phar/
+curl -s -o start.sh https://github.com/pmmp/PocketMine-MP/releases/download/{PMMP_VER}/start.sh/
+chmod +x start.sh
+echo -e "[*] Everything done! Run ./start.sh to start PocketMine-MP"
+exit 0
