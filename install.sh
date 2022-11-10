2314a10bc7a7b0bc7074d9377e1acb3b003c2dc0
Nguyễn Hồng Sơn <81855366+crischutu07@users.noreply.github.com>
2022-11-10 07:17:17 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
+## Checking commands if it exists 
 if ! command -v curl &> /dev/null; then
   echo "[*] Command curl not found"
   exit 1
@@ -20,14 +21,15 @@ if [ `getconf LONG_BIT` == "32" ]; then
         echo "[*] PocketMine-MP is no longer supported on 32-bit systems."
         exit 1
 fi
-if [[ "$TERMUX_VERSION" < "0.118.0" ]]; then
+# Check user if Termux Version is not 0.118.0
+if [[ "$TERMUX_VERSION" == "0.118.0" ]]; then
   echo -e "Please use the lastest version of Termux\nFor more Information: https://github.com/termux/termux-app#Installation"
   exit 1
 fi
 # Export variable after checking command
 CHANNEL=$(curl -s https://update.pmmp.io/api | jq -r ".channel")
 CHANNEL_QUOTE=$(curl -s https://update.pmmp.io/api | jq ".channel")
-PHP_VER="8.0.22"
+PHP_VER="8.0.22" 
 echo -e "[*] Retrieving latest build data for channel ${CHANNEL_QUOTE}"
 PMMP_VER=$(curl -s https://update.pmmp.io/api | jq -r ".base_version")
 MCPE_VER=$(curl -s https://update.pmmp.io/api | jq -r ".mcpe_version")
@@ -39,13 +41,13 @@ echo -e "[*] This stable build was released on $DATE_CONVERT"
 echo -e "[*] Found PocketMine-MP ${PMMP_VER} (build ${BUILD}) for Minecraft: PE v${MCPE_VER} (PHP ${PHP_PMMP})"
 echo "[*] Installing/updating PocketMine-MP on directory ./"
 mkdir -p ./bin/php7/bin/
-
+## Install php binary for ARM Device
 wget -q -O php https://github.com/DaisukeDaisuke/AndroidPHP/releases/download/${PHP_VER}/php
 mv php ./bin/php7/bin
 wget -q -O php.ini https://github.com/DaisukeDaisuke/AndroidPHP/releases/download/${PHP_VER}/php-pm4.ini
 mv php.ini ./bin/php7/bin/
 chmod +x ./bin/php7/bin/php
-
+# Install PMMP
 wget -q -O PocketMine-MP.phar https://github.com/pmmp/PocketMine-MP/releases/download/${PMMP_VER}/PocketMine-MP.phar
 wget -q -O start.sh https://raw.githubusercontent.com/pmmp/PocketMine-MP/${PMMP_VER}/start.sh
 chmod +x start.sh
